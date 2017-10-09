97f3726d43bccf578f6dc92dca7ced24675d4cbe
LMacchi <lau.macchi@gmail.com>
2017-10-09 16:00:13 -0700

    Change default_realm using augeas

--- site/kerberos/manifests/init.pp
+++ site/kerberos/manifests/init.pp
@@ -1,6 +1,6 @@
 class kerberos {
   augeas { 'change default realm in krb5.conf':
-    context => '/files/etc/krb5.conf/libdefaults/default_realm',
+    context => '/files/etc/krb5.conf/libdefaults',
     changes => 'set default_realm PUPPETLABS.VM',
   }
 }
